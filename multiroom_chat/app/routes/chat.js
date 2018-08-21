418c747feac731a219e8355497e474fcc4b8d9b1
CrazyAMK <eduardo-amroginski@hotmail.com>
2018-08-21 01:02:52 -0300

    Servidor configurado e front end estabelecido, Seção 13, aula 60

---
+++ multiroom_chat/app/routes/chat.js
@@ -0,0 +1,10 @@
+module.exports = function(application){
+    application.post('/chat', function(req, res){
+        res.render('chat');
+    });
+
+    application.get('/chat', function(req, res){
+        res.render('chat');
+    });
+    
+}
